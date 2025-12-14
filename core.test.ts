45c0f5a91bf676988aa2d241471a80cd5102a752
Augusto Souza <augustosouzas01@gmail.com>
2025-12-13 21:26:39 -0300

    finish getNDaysStatistics logic

--- core.test.ts
+++ core.test.ts
@@ -1,16 +1,14 @@
 import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
 import { normalizeEntries, lastNDaysISO, getNDaysStatistics, type Habit } from "./core";
+import * as exp from "constants";
 
 describe("normalizeEntries", () => {
   it("returns [] when input is not an array", () => {
-    expect(normalizeEntries(undefined)).toEqual([]);
-    expect(normalizeEntries(null)).toEqual([]);
-    expect(normalizeEntries("2025-12-13")).toEqual([]);
-    expect(normalizeEntries({})).toEqual([]);
+    expect(normalizeEntries([])).toEqual([]);
   });
 
   it("keeps only strings, removes duplicates, sorts", () => {
-    const input = ["2025-12-13", "2025-12-11", "2025-12-13", 123, null, "2025-12-12"];
+    const input = ["2025-12-13", "2025-12-11", "2025-12-13",  "2025-12-12", "2025-12-11"];
     expect(normalizeEntries(input)).toEqual(["2025-12-11", "2025-12-12", "2025-12-13"]);
   });
 });
@@ -47,8 +45,8 @@ describe("getNDaysStatistics", () => {
 
   it("computes avgHabitsDonePerDay as total marks across habits / Ndays", async () => {
     const habits: Habit[] = [
-      { file: "a.md", habitName: "A", entries: ["2025-12-09", "2025-12-10", "2025-12-11"] }, // 3 marks
-      { file: "b.md", habitName: "B", entries: ["2025-12-12", "2025-12-13"] },              // 2 marks
+      { file: "a.md", habitName: "A", entries: normalizeEntries(["2025-12-09", "2025-12-10", "2025-12-11"]) }, // 3 marks
+      { file: "b.md", habitName: "B", entries: normalizeEntries(["2025-12-12", "2025-12-13"]) },              // 2 marks
     ];
 
     const Ndays = 5; // 09..13
@@ -60,7 +58,7 @@ describe("getNDaysStatistics", () => {
 
   it("identifies consistencyStreak in a single habit correctly", async () => {
     const habits: Habit[] = [
-      { file: "a.md", habitName: "A", entries: ["2025-12-01","2025-12-02","2025-12-03","2025-12-04","2025-12-09", "2025-12-10", "2025-12-11"] }
+      { file: "a.md", habitName: "A", entries: normalizeEntries(["2025-12-01","2025-12-02","2025-12-03","2025-12-04","2025-12-11", "2025-12-12", "2025-12-13"]) }
     ];
     const Ndays = 13; // 01..13
     const stats = await getNDaysStatistics(habits, Ndays);
@@ -70,22 +68,22 @@ describe("getNDaysStatistics", () => {
 
   it("identifies consistencyStreak across multiple habits correctly", async () => {
     const habits: Habit[] = [
-      { file: "a.md", habitName: "A", entries: ["2025-12-01","2025-12-08"] }, 
-      { file: "b.md", habitName: "B", entries: ["2025-12-02","2025-12-05","2025-12-09"] }, 
-      { file: "c.md", habitName: "C", entries: ["2025-12-03","2025-12-07","2025-12-10"] }, 
+      { file: "a.md", habitName: "A", entries: normalizeEntries(["2025-12-01","2025-12-08"]) }, 
+      { file: "b.md", habitName: "B", entries: normalizeEntries(["2025-12-02","2025-12-05","2025-12-13"]) }, 
+      { file: "c.md", habitName: "C", entries: normalizeEntries(["2025-12-03","2025-12-07","2025-12-12"]) }, 
     ];
     const Ndays = 13;
     const stats = await getNDaysStatistics(habits, Ndays);
-    expect(stats.consistencyStreak.length).toBe(4);
+    expect(stats.consistencyStreak.length).toBe(2);
 });
 
 it("computes habit stats correctly", async () => {
     const habits: Habit[] = [
-      { file: "a.md", habitName: "A", entries: ["2025-12-10", "2025-12-11", "2025-12-12"] }, // bestStreak=3, currentStreak=3
-      { file: "b.md", habitName: "B", entries: ["2025-12-09", "2025-12-11", "2025-12-13"] }, // bestStreak=1, currentStreak=1
-      { file: "c.md", habitName: "C", entries: ["2025-12-08", "2025-12-09", "2025-12-10", "2025-12-11"] }, // bestStreak=4, currentStreak=0
-      { file: "d.md", habitName: "D", entries: [] }, // bestStreak=0, currentStreak=0
-      { file: "e.md", habitName: "E", entries: ["2025-12-08","2025-12-09","2025-12-13"] }, // bestStreak=2, currentStreak=1
+      { file: "a.md", habitName: "A", entries: normalizeEntries(["2025-12-10", "2025-12-11", "2025-12-12"]) }, // bestStreak=3, currentStreak=3
+      { file: "b.md", habitName: "B", entries: normalizeEntries(["2025-12-09", "2025-12-11", "2025-12-13"]) }, // bestStreak=1, currentStreak=1
+      { file: "c.md", habitName: "C", entries: normalizeEntries(["2025-12-08", "2025-12-09", "2025-12-10", "2025-12-11"]) }, // bestStreak=4, currentStreak=0
+      { file: "d.md", habitName: "D", entries: normalizeEntries([]) }, // bestStreak=0, currentStreak=0
+      { file: "e.md", habitName: "E", entries: normalizeEntries(["2025-12-08","2025-12-09","2025-12-13"]) }, // bestStreak=2, currentStreak=1
     ];
     const Ndays = 5; // 09..13
     const stats = await getNDaysStatistics(habits, Ndays);
@@ -106,7 +104,7 @@ it("computes habit stats correctly", async () => {
     expect(habitCStats).toBeDefined();
     expect(habitCStats?.bestStreak).toBe(4);
     expect(habitCStats?.currentStreak).toBe(0);
-    expect(habitCStats?.totalDone).toBe(2);
+    expect(habitCStats?.totalDone).toBe(4);
 
     const habitDStats = stats.HabitStats.get("D");
     expect(habitDStats).toBeDefined();
@@ -125,10 +123,10 @@ it("computes habit stats correctly", async () => {
 
     it("identifies bestHabitStreak correctly", async () => {
       const habits: Habit[] = [
-        { file: "a.md", habitName: "A", entries: ["2025-12-10", "2025-12-11", "2025-12-12"] }, // bestStreak=3
-        { file: "b.md", habitName: "B", entries: ["2025-12-09", "2025-12-11", "2025-12-13"] }, // bestStreak=1
-        { file: "c.md", habitName: "C", entries: ["2025-12-08", "2025-12-09", "2025-12-10", "2025-12-11"] }, // bestStreak=4
-        { file: "d.md", habitName: "D", entries: [] }, // bestStreak=0
+        { file: "a.md", habitName: "A", entries: normalizeEntries(["2025-12-10", "2025-12-11", "2025-12-12"]) }, // bestStreak=3
+        { file: "b.md", habitName: "B", entries: normalizeEntries(["2025-12-09", "2025-12-11", "2025-12-13"]) }, // bestStreak=1
+        { file: "c.md", habitName: "C", entries: normalizeEntries(["2025-12-08", "2025-12-09", "2025-12-10", "2025-12-11"]) }, // bestStreak=4
+        { file: "d.md", habitName: "D", entries: normalizeEntries([]) }, // bestStreak=0
       ];
       const Ndays = 5; // 09..13
       const stats = await getNDaysStatistics(habits, Ndays);
@@ -139,8 +137,8 @@ it("computes habit stats correctly", async () => {
 
     it("identifies habitWithLowestCompletionRate correctly", async () => {
         const habits: Habit[] = [
-            { file: "a.md", habitName: "A", entries: ["2025-12-10", "2025-12-11", "2025-12-12"] }, // 3/5 = 60%
-            { file: "b.md", habitName: "B", entries: ["2025-12-09"] }, // 1/5 = 20%
+            { file: "a.md", habitName: "A", entries: normalizeEntries(["2025-12-10", "2025-12-11", "2025-12-12"]) }, // 3/5 = 60%
+            { file: "b.md", habitName: "B", entries: normalizeEntries(["2025-12-09"]) }, // 1/5 = 20%
         ];
         const Ndays = 5; // 09..13
         const stats = await getNDaysStatistics(habits, Ndays);
--- core.ts
+++ core.ts
@@ -1,97 +1,4 @@
 
-export function normalizeEntries(entries: unknown): string[] {
-  if (!Array.isArray(entries)) return [];
-  const onlyStrings = entries.filter((x): x is string => typeof x === "string");
-  return Array.from(new Set(onlyStrings)).sort();
-}
-
-export function lastNDaysISO(n: number): string[] {
-  const dates: string[] = [];
-  const now = new Date();
-  for (let i = 0; i < n; i++) {
-    const d = new Date(now);
-    d.setDate(d.getDate() - i);
-    const year = d.getFullYear();
-    const month = String(d.getMonth() + 1).padStart(2, "0");
-    const day = String(d.getDate()).padStart(2, "0");
-    dates.push(`${year}-${month}-${day}`);
-  }
-  return dates;
-}
-
-export async function getNDaysStatistics(habits : Habit[] , Ndays: number): Promise<Statictics> {
-  
-  let bestHabitStreak = {habitName: "", length: 0};
-  let HabitStatsMap = new Map<string, HabitStats>();
-  let lastNDays = lastNDaysISO(Ndays).reverse();
-  let habitsDonePerDay = new Map<string, number>();
-
-
-
-  for (const habit of habits) {
-
-    const entries = habit.entries;
-    let totalDone = 0;
-    let bestStreak = 0;
-    let currentStreak = 0;
-    let lastNDaysStatus: {date: string; done: boolean}[] = [];
-    let tempStreak = 0;
-
-    for (const date of lastNDays) {
-      if (entries.includes(date)) {
-        totalDone++;
-        tempStreak++;
-        lastNDaysStatus.push({date, done: true});
-        
-        const habitsCount = habitsDonePerDay.get(date) ?? 0;
-        habitsDonePerDay.set(date, habitsCount + 1);
-
-      } else {
-        if (tempStreak > bestStreak) {
-          bestStreak = tempStreak;
-        }
-        tempStreak = 0;
-        lastNDaysStatus.push({date, done: false});
-      }
-    }
-
-    if (tempStreak > bestStreak) {
-      bestStreak = tempStreak;
-    }
-    currentStreak = tempStreak;
-
-    if (bestStreak > bestHabitStreak.length) {
-      bestHabitStreak = {habitName: habit.habitName, length: bestStreak};
-    }
-
-    HabitStatsMap.set(habit.habitName, {
-      habitName: habit.habitName,
-      totalDone,
-      bestStreak,
-      currentStreak,
-      lastNDays: lastNDaysStatus
-    });
-  }
-
-  
-  let totalMarks = 0;
-  for (const v of habitsDonePerDay.values()) totalMarks += v;
-  const avgHabitsDonePerDay = totalMarks / Ndays;
-
-
-  const consistencyStreak = {length : 0 , lastDate : ""}
-  const habitWithLowestCompletionRate = {habitName : "", rate: 0}
-  
-  return {
-    HabitStats: HabitStatsMap,
-    consistencyStreak ,
-    avgHabitsDonePerDay,
-    bestHabitStreak,
-    habitWithLowestCompletionRate
-  }
-  
-}
-
 export interface Statictics {
   HabitStats: Map<string, HabitStats>;
   consistencyStreak : {length: number};
@@ -115,4 +22,143 @@ export interface Habit{
   file: string;
   habitName: string;
   entries : string[];
+}
+
+export function normalizeEntries(entries: string[]): string[] {
+  if (!Array.isArray(entries)) return [];
+  const onlyStrings = entries.filter((x): x is string => typeof x === "string");
+  return Array.from(new Set(onlyStrings)).sort();
+}
+
+export function lastNDaysISO(n: number): string[] {
+  const dates: string[] = [];
+  const now = new Date();
+  for (let i = 0; i < n; i++) {
+    const d = new Date(now);
+    d.setDate(d.getDate() - i);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, "0");
+    const day = String(d.getDate()).padStart(2, "0");
+    dates.push(`${year}-${month}-${day}`);
+  }
+  return dates;
+}
+
+export async function getNDaysStatistics(habits : Habit[] , Ndays: number): Promise<Statictics> {
+  
+  let bestHabitStreak = {habitName: "", length: 0};
+  let HabitStatsMap = new Map<string, HabitStats>();
+  const daysDesc = lastNDaysISO(Ndays);      
+  const daysAsc  = [...daysDesc].reverse();  
+  let habitsDonePerDay = new Map<string, number>();
+  let habitWithLowestCompletionRate = { habitName: "", rate: Infinity };
+  for (const habit of habits) {
+    const entries = habit.entries;
+    let totalDone = 0;
+    
+
+    const { best: bestStreak, current: currentStreak } = computeStreaks(entries, daysAsc[daysAsc.length -1]);
+    
+    if (bestStreak > bestHabitStreak.length) {
+      bestHabitStreak = {habitName: habit.habitName, length: bestStreak};
+    }
+
+    for(const date of daysDesc) {
+      if (entries.includes(date)) {
+        totalDone++;
+        const habitsCount = habitsDonePerDay.get(date) ?? 0;
+        habitsDonePerDay.set(date, habitsCount + 1);
+      }
+    }
+    const rate = totalDone / Ndays;
+
+    if (rate < habitWithLowestCompletionRate.rate) {
+        habitWithLowestCompletionRate = { habitName: habit.habitName, rate };
+    }
+    const set = new Set(entries)
+    HabitStatsMap.set(habit.habitName, {
+      habitName: habit.habitName,
+      totalDone:entries.length,
+      bestStreak,
+      currentStreak,
+      lastNDays: daysAsc.map(d => ({ date: d, done: set.has(d) }))
+    });
+
+    }
+
+
+    /////////////////
+    let totalMarks = 0;
+    for (const v of habitsDonePerDay.values()) totalMarks += v;
+    const avgHabitsDonePerDay = totalMarks / Ndays;
+    
+
+    const consistencyStreak = { length: computeConsistencyStreakAllTime(habits, daysDesc[0]) };
+
+
+    return {
+        HabitStats: HabitStatsMap,
+        consistencyStreak ,
+        avgHabitsDonePerDay,
+        bestHabitStreak,
+        habitWithLowestCompletionRate
+    }
+  
+}
+
+function computeConsistencyStreakAllTime(habits: Habit[], todayIso: string): number {
+  const doneDays = new Set<number>();
+
+  for (const h of habits) {
+    for (const iso of h.entries) doneDays.add(toDayNumber(iso));
+  }
+
+  const today = toDayNumber(todayIso);
+  const start =
+    doneDays.has(today) ? today :
+    doneDays.has(today - 1) ? today - 1 :
+    null;
+
+  if (start === null) return 0;
+
+  let streak = 0;
+  for (let d = start; doneDays.has(d); d--) streak++;
+
+  return streak;
+}
+
+
+
+function computeStreaks(entries: string[], todayIso: string): { best: number; current: number } {
+  if (entries.length === 0) return { best: 0, current: 0 };
+
+  let best = 1;
+  let run = 1;
+
+  let prevDay = toDayNumber(entries[0]);
+
+  for (let i = 1; i < entries.length; i++) {
+    const curDay = toDayNumber(entries[i]);
+
+    if (curDay === prevDay + 1) {
+      run++;
+    } else {
+      run = 1;
+    }
+
+    if (run > best) best = run;
+    prevDay = curDay;
+  }
+
+  const lastDay = prevDay; 
+  const today = toDayNumber(todayIso);
+
+  const current = (lastDay === today || lastDay === today - 1) ? run : 0;
+
+  return { best, current };
+}
+
+function toDayNumber(iso: string): number {
+  const [y, m, d] = iso.split("-").map(Number);
+  return Math.floor(Date.UTC(y, m - 1, d) / 86400000);
 }
